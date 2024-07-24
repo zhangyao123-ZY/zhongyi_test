7aafb266e718c4457a40d0de27c8ec73dc42c686
zhang <1824715008@qq.com>
2024-07-24 18:51:12 +0800

    fix: app

--- apps/ecms/src/App.vue
+++ apps/ecms/src/App.vue
@@ -46,7 +46,8 @@ const isShowRouterView = computed(() => {
  */
 const videiotApi = VideIotServer.getInstance();
 // "http://192.168.199.220:5173/api"
-const baseUrl: string = `${location.protocol}//${location.host}/api`;
+// const baseUrl: string = `${location.protocol}//${location.host}/api`;
+const baseUrl: string = `https://pro-api.anytalk.agrtc.cn`;
 videiotApi.init(baseUrl, {
   RSApriKey: 'MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAMvNqw+s52/LwqJXRbnOBw0PAhUTQdLGBEz4UniaqBoCsHiJU2WRRYc9kE4q/RTk43kryzZVRN/SFCkVz8mLW4MlC5eWGxDGxepNHvEeFEZI8HDzkM1ClpmEN7RBFXk1zvzFAOGZvAgZgP5U6g4U0n+k5mM5TqDLjdNhHvrvqHOlAgMBAAECgYEAypcw840iJvy7BtuQQUpvseh4XnDdHuOehNDYUnESp4CC4d9I2o8VOYHWQA31tF5vTRVelS0gIQnY5pp7ClMQbD/5ZldJM5tHctlYfVKxBvbiHpumNZjcwfEmWOh3/vG7gQmvvuzYc8pWygPdHms304zvAyjvUc974jjUBIR6NYECQQDsH/YPDEvWo3fzGPpjOkRoEYxTa6o5rdg/r0Y4ImO6//l/5FYyLP1UzP5EDIg9S+Yff6f8S7iczMh5rkV2pLkFAkEA3PU90Ntd8CRnMkc7PMUSaRTJBm/BLCHsVRBCX8oi2GPDiBGBBC6orNsvQOpnIYXLh6GOiCGTCZHYexvbMNVSIQJBAMsW8lwfB1ulXhYI9dNC2nNoJHnbsJ3LmKOt7Yd6jKzaXY5SppChgYLejhOZc9TE4zlUYvftaOv0drmx2q0hIMECQQCA7RgEpftKA/OzAp0t+E+lwNsLRmoQtjqP58zS1789Q/jzjEMbjZ4ePkZ0OlGy3b7EhSAFjpGdI9yM+NQMymwBAkAH1g76XJzBzvGEzT/eSRX0s6s7D0ZBpMduUtKcIB+R/NbJf8SGBjGmoLAdY8bKNHwX0+ICuMu4OuqT0rLWVbKi',
   // 响应拦截
